8a8bab105625ac9f228f7ea88039921ae2159510
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-09-19 13:53:47 +0200

    First test

--- prefixopt_derive/tests/enums.rs
+++ prefixopt_derive/tests/enums.rs
@@ -7,7 +7,7 @@ extern crate clap;
 #[test]
 fn named_enum() {
     use prefixopt::*;
-    #[derive(PrefixOpt, Debug)]
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
     pub enum A {
         A(u32, u32),
         B{x: u64, y: u64},
@@ -19,5 +19,7 @@ fn named_enum() {
     }
     let a_opt = A::with_prefix("o");
     let app = a_opt.as_arguments().bind_app(clap::App::new("named_enum"));
-    a_opt.override_arguments(A::B{x:5,y:6}).get_matches_from_safe(&["test", "--o.B.x=2"]);
+    let matches = app.get_matches_from_safe(&["test", "--o.B.x=2"]);
+    let a = a_opt.override_arguments(A::B{x:5,y:6}, &matches.unwrap());
+    assert_eq!(a, Some(A::B{x:2, y:6}));
 }
