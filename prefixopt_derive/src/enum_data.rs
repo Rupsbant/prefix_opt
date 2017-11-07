62610a9bb4f7595d8444e672c1be6becb0ff9257
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-11-07 18:45:47 +0100

    Rewriting enum handling to support generics
    Removing temporary structs and directly inlining sub objects

--- prefixopt_derive/src/enum_data.rs
+++ prefixopt_derive/src/enum_data.rs
@@ -1,29 +1,13 @@
 use super::*;
 
-pub fn derive(generics: &Generics, ident: &Ident, variant: &Vec<Variant>) -> quote::Tokens {
-    let (idents, structs): (Vec<(_, _)>, Vec<_>) = variant
-        .iter()
-        .map(|var| {
-            let struct_ident = Ident::new(format!("{}_{}", ident, var.ident));
-            let decl_struct = decl_struct(generics, &struct_ident, &var.data);
-            let impl_prefix = super::variant_data::derive(&generics, &struct_ident, &var.data);
-            let impl_from = impl_from(generics, &ident, &var.ident, &struct_ident, &var.data);
-            ((&var.ident, struct_ident),
-             quote!(
-                     #decl_struct
-                     #impl_prefix
-                     #impl_from
-                 ))
-        })
-        .unzip();
-    let ident_container = Ident::new(format!("PREFIXOPT_FOR_{}", ident));
-    let decl_enum = decl_enum(generics, &ident_container, &idents);
-    let with_prefix = impl_with_prefix(&idents);
-    let as_arguments = impl_as_arguments(&idents);
-    let match_arguments = impl_match_arguments(&idents);
+pub fn derive(gen: &Generics, ident: &Ident, variant: &Vec<Variant>) -> quote::Tokens {
     let dummy = Ident::new(format!("_IMPL_PREFIXOPT_FOR_{}", ident));
-
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    let mut collect = CollectVariants::empty();
+    for var in variant {
+        let one = OneVariant::build(var);
+        collect.append_variant(ident, &var.ident, &one);
+    }
+    let impl_tokens = collect.finish(ident, gen);
     quote!(
         #[allow(non_upper_case_globals)]
         #[allow(unused_attributes, unused_imports, unused_variables)]
@@ -31,167 +15,185 @@ pub fn derive(generics: &Generics, ident: &Ident, variant: &Vec<Variant>) -> quo
             extern crate prefixopt;
             use prefixopt::*;
             use prefixopt::concat_ref::*;
-            #decl_enum
-            impl #impl_generics PrefixOptContainer for #ident_container #ty_generics #where_clause  {
-                type Parsed = #ident #ty_generics;
-                #with_prefix
-                #as_arguments
-                #match_arguments
-            }
-            impl #impl_generics PrefixOpt for #ident #ty_generics #where_clause  {
-                type Container = #ident_container #ty_generics;
-            }
-            #(#structs)*
+            #impl_tokens
         };
     )
 }
-
-fn decl_enum(generics: &Generics, ident: &Ident, tags: &[(&Ident, Ident)]) -> quote::Tokens {
-    let (impl_generics, ty_generics, _) = generics.split_for_impl();
-    let ty_generics = ::std::iter::repeat(&ty_generics);
-    let fname = tags.iter().map(|id| &id.0);
-    let ftype = tags.iter().map(|id| &id.1);
-    let group = tags.iter()
-        .map(|&(ref id, _)| Ident::new(format!("{}_group", id)));
-    quote!(
-        #[allow(non_camel_case_types)]
-        #[allow(non_snake_case)]
-        pub struct #ident #impl_generics {
-            #(
-              #fname: <#ftype #ty_generics as PrefixOpt>::Container,
-              #group: String,)*
-        }
-    )
+#[derive(Default)]
+pub struct OneVariant {
+    struct_fields: quote::Tokens,
+    construct_concat: quote::Tokens,
+    add_arguments: Vec<quote::Tokens>,
+    fields_match: quote::Tokens,
+    fields_tuple: quote::Tokens,
+    override_arguments: quote::Tokens,
 }
 
-fn impl_with_prefix(tags: &[(&Ident, Ident)]) -> quote::Tokens {
-    let tname = tags.iter().map(|id| id.0);
-    let ttype = tags.iter().map(|id| &id.1);
-    let tfmts1 = tags.iter().map(|id| id.0.as_ref());
-    let tfmts2 = tags.iter().map(|id| format!("{}_group", id.0));
-    let group = tags.iter()
-        .map(|id| Ident::new(format!("{}_group", id.0)));
-    quote!(
-        #[allow(non_snake_case)]
-        fn concat_prefix(prefix: &ConcatRef<&Display>) -> Self {
-            Self {
-                #(#tname: <#ttype as PrefixOpt>::Container::concat_prefix(&prefix.append(&#tfmts1)),)*
-                #(#group: prefix.append(&#tfmts2).into(),)*
-            }
-        }
-    )
+#[derive(Default)]
+pub struct CollectVariants {
+    struct_fields: quote::Tokens,
+    construct_concat: quote::Tokens,
+    add_groups: quote::Tokens,
+    group_conflicts: quote::Tokens,
+    add_arguments: quote::Tokens,
+    override_arguments: quote::Tokens,
 }
-fn impl_as_arguments(tags: &[(&Ident, Ident)]) -> quote::Tokens {
-    let group = tags.iter()
-        .map(|id| Ident::new(format!("{}_group", id.0)))
-        .collect::<Vec<_>>();
-    let group_ref = &group;
-    let (g1, g2):(Vec<_>, Vec<_>) = (0..group.len())
-        .into_iter()
-        .map(|idx: usize| {
-            let subslice = &group_ref[0..idx.clone()];
-            (&group_ref[idx], subslice.iter())
-        })
-        .unzip();
-    let groups = &group;
-    let t1 = tags.iter().map(|id| &id.0);
-    quote!(
-        fn as_arguments(&self) -> Args {
-            let mut o = Args::default()#(
-                .add_group(
-                    clap::ArgGroup::with_name(&self.#g1)
-                        .multiple(true)
-                        #(.conflicts_with(&self.#g2))*
-                )
-            )*;
-            #(o.extend(self.#t1.as_arguments().map_arg(|arg| arg.group(&self.#groups)));)*
-            o
-        }
-    )
-}
-fn impl_match_arguments(tags: &[(&Ident, Ident)]) -> quote::Tokens {
-    let tname = tags.iter().map(|id| id.0);
-    let ttype = tags.iter().map(|id| &id.1);
-    let group = tags.iter()
-        .map(|id| Ident::new(format!("{}_group", id.0)))
-        .collect::<Vec<_>>();
-    quote!(
-        fn override_arguments(&self, parsed: Self::Parsed, matches: &clap::ArgMatches) -> Option<Self::Parsed> {
-            #(if matches.is_present(&self.#group) {
-                let unwrap = #ttype::from(parsed);
-                self.#tname.override_arguments(unwrap, matches).map(From::from)
-            }else)* {
-                Some(Self::Parsed::default())
-            }
-
-        }
-    )
-}
-
-fn impl_from(generics: &Generics, enu: &Ident, name: &Ident, ty: &Ident, var_data: &VariantData) -> quote::Tokens {
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-    let (construct, matcher, match_constructor) = match *var_data {
-        VariantData::Struct(ref v) => {
-            let v1 = v.iter().map(|f| &f.ident);
-            let v2 = v.iter().map(|f| &f.ident);;
-            let construct = quote!({#(#v2: fr.#v1,)*});
-            let v1 = v.iter().map(|f| &f.ident);
-            let matcher = quote!({#(#v1,)*});
-            (construct,
-            matcher.clone(), matcher)
-        }
-        VariantData::Tuple(ref v) =>{
-            let id = v.iter().enumerate().map(|(idx,_)| Ident::new(idx.to_string()));
-            let construct = quote!((#(fr.#id,)*));
-            let letters1 = v.iter().enumerate().map(|(idx,_)| Ident::new(format!("a{}", idx)));
-            let matcher = quote!((#(#letters1,)*));
-            (construct, matcher.clone(), matcher)
-        }
-        VariantData::Unit => (quote!(), quote!(), quote!(()))
-    };
-    quote!(
-        impl #impl_generics From<#ty #ty_generics> for #enu #ty_generics #where_clause {
-            fn from(fr: #ty #ty_generics) -> Self {
-                #enu::#name #construct
-            }
-        }
-        impl#impl_generics #ty #ty_generics #where_clause {
-            fn from(fr: #enu#ty_generics) -> Self {
-                match fr {
-                    #enu::#name#matcher => #ty#match_constructor,
-                    _ => #ty::default(),
-                }
-            }
-        }
-    )
-}
-
-fn decl_struct(gen: &Generics, ident: &Ident, variant_data: &VariantData) -> quote::Tokens {
-    let (_, ty_generics, _) = gen.split_for_impl();
-    match *variant_data {
-        VariantData::Struct(ref fields) => {
-            let names = fields.iter().map(|f| f.ident.as_ref().unwrap());
-            let types = fields.iter().map(|f| &f.ty);
+impl OneVariant {
+    fn build(var: &Variant) -> OneVariant {
+        let (f_ids, f_dots): (Vec<_>, Vec<_>) = var.data
+            .fields()
+            .iter()
+            .enumerate()
+            .map(|(i, ident)| {
+                let display = ident
+                    .ident
+                    .as_ref()
+                    .map(|id| id as &::std::fmt::Display)
+                    .unwrap_or(&i);
+                let id = Ident::new(format!("{}_{}", var.ident, display));
+                let dot = format!("{}.{}", var.ident, display);
+                (id, dot)
+            })
+            .unzip();
+        let ty = var.data
+            .fields()
+            .iter()
+            .map(|f| &f.ty)
+            .collect::<Vec<_>>();
+        let f_ids = &f_ids;
+        let ty = &ty;
+        let concat = quote!(#(#f_ids: <#ty as PrefixOpt>::Container::concat_prefix(&prefix.append(&#f_dots)),)*);
+        let override_arguments = {
+            let f_ids1 = f_ids;
+            let f_ids2 = f_ids;
+            let f_ids3 = f_ids;
             quote!(
-                #[allow(non_camel_case_types)]
-                #[derive(Debug, Default)]
-                pub struct #ident #ty_generics {
-                    #(#names: #types,)*
+                #(let #f_ids1 = if let Some(p) = self.#f_ids2.override_arguments(#f_ids3, matches) {
+                    p} else {return None};)*)
+        };
+        let add_arguments = f_ids
+            .iter()
+            .map(|f_id| quote!(self.#f_id.as_arguments()))
+            .collect::<Vec<_>>();
+        let (tuple, matcher) = {
+            let sep = quote!(#(#f_ids,)*);
+            match var.data {
+                VariantData::Struct(_) => {
+                    let f = var.data
+                        .fields()
+                        .iter()
+                        .map(|f| f.ident.as_ref().unwrap());
+                    (quote!((#sep)), quote!({#(#f : #f_ids,)*}))
                 }
-            )
+                VariantData::Tuple(_) => (quote!((#sep)), quote!((#sep))),
+                VariantData::Unit => (quote!(()), quote!()),
+            }
+        };
+        OneVariant {
+            struct_fields: quote!(#(#f_ids: <#ty as PrefixOpt>::Container,)*),
+            construct_concat: concat,
+            add_arguments: add_arguments,
+            fields_match: matcher,
+            fields_tuple: tuple,
+            override_arguments: override_arguments,
         }
-        VariantData::Tuple(ref fields) => {
-            let types = fields.iter().map(|f| &f.ty);
-            quote!(
-                #[allow(non_camel_case_types)]
-                #[derive(Debug, Default)]
-                pub struct #ident #ty_generics(#(#types,)*);
-            )
+    }
+}
+impl CollectVariants {
+    fn empty() -> CollectVariants {
+        Default::default()
+    }
+    fn append_variant(&mut self, enu: &Ident, discriminant: &Ident, var: &OneVariant) {
+        let group = Ident::new(format!("{}_group", discriminant));
+        let discr_str = discriminant.as_ref();
+        let group_str = group.as_ref();
+        self.struct_fields.append(&var.struct_fields);
+        self.struct_fields
+            .append(quote!(
+            #discriminant: String,
+            #group: String,));
+        self.construct_concat.append(&var.construct_concat);
+        self.construct_concat
+            .append(quote!(
+            #discriminant: prefix.append(&#discr_str).into(),
+            #group: prefix.append(&#group_str).into(),
+        ));
+        {
+            let group_conflicts = &self.group_conflicts;
+            self.add_groups
+                .append(quote!(
+            .add_group(
+                clap::ArgGroup::with_name(&self.#group)
+                    .multiple(true)
+                    #group_conflicts)
+        ));
         }
-        VariantData::Unit => quote!(
+        self.group_conflicts
+            .append(quote!(.conflicts_with(&self.#group)));
+        let add_args = &var.add_arguments;
+        {
+            let group_rep = ::std::iter::repeat(&group);
+            self.add_arguments
+                .append(quote!(
+            #(o.extend(#add_args.map_arg(|arg| arg.group(&self.#group_rep)));)*
+            o.add_arg(clap::Arg::with_name(&self.#discriminant).long(&self.#discriminant).group(&self.#group));
+        ));
+        }
+        let override_arguments = &var.override_arguments;
+        let fields_match = &var.fields_match;
+        let fields_tuple = &var.fields_tuple;
+        self.override_arguments
+            .append(quote!(
+            if matches.is_present(&self.#group) {
+                let #fields_tuple = match parsed {
+                    #enu::#discriminant#fields_match => #fields_tuple,
+                    _ => Default::default(),
+                };
+                #override_arguments;
+                Some(#enu::#discriminant#fields_match)
+            } else
+        ))
+    }
+    fn finish(self, enum_name: &Ident, gen: &Generics) -> quote::Tokens {
+        let ident_container = Ident::new(format!("PREFIXOPT_FOR_{}", enum_name));
+        let (impl_generics, ty_generics, where_clause) = gen.split_for_impl();
+        let struct_fields = self.struct_fields;
+        let construct_concat = self.construct_concat;
+        let add_groups = self.add_groups;
+        let add_arguments = self.add_arguments;
+        let override_arguments = self.override_arguments;
+        quote!(
             #[allow(non_camel_case_types)]
-            #[derive(Debug, Default)]
-            pub struct #ident();
-        ),
+            #[allow(non_snake_case)]
+            pub struct #ident_container #impl_generics {
+                #struct_fields
+            }
+            impl #impl_generics PrefixOptContainer for #ident_container
+                    #ty_generics #where_clause {
+                type Parsed = #enum_name #ty_generics;
+                #[allow(non_snake_case)]
+                fn concat_prefix(prefix: &ConcatRef<&Display>) -> Self {
+                    Self {#construct_concat}
+                }
+                #[allow(non_snake_case)]
+                fn as_arguments(&self) -> Args {
+                    let mut o = Args::default()#add_groups;
+                    #add_arguments;
+                    o
+
+                }
+                #[allow(non_snake_case)]
+                fn override_arguments(&self, parsed: Self::Parsed, matches: &clap::ArgMatches) -> Option<Self::Parsed> {
+                    #override_arguments {
+                        Some(parsed)
+                  }
+                }
+
+            }
+            impl #impl_generics PrefixOpt for #enum_name
+                    #ty_generics #where_clause {
+                type Container = #ident_container #ty_generics;
+            }
+        )
     }
 }
