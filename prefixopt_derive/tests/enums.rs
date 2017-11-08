eea3c26b807f492b1f8f154ed999744d62f898ab
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-11-08 12:06:19 +0100

    More testing

--- prefixopt_derive/tests/enums.rs
+++ prefixopt_derive/tests/enums.rs
@@ -3,23 +3,88 @@
 extern crate prefixopt_derive;
 extern crate prefixopt;
 extern crate clap;
-
-#[test]
-fn named_enum() {
+macro_rules! run_test {
+    ( $ty:ty, $inp: expr, $( $args:expr),*) => {
+        {
+                let a_opt = <$ty as PrefixOpt>::with_prefix("o");
+                let app = a_opt
+                    .as_arguments()
+                    .bind_app(clap::App::new("named_enum"));
+                let matches = app.get_matches_from_safe(&["test", $($args,)*]);
+                a_opt.override_arguments($inp, &matches.unwrap())
+        }
+    }
+}
+mod simple {
     use prefixopt::*;
     #[derive(PrefixOpt, Debug, PartialEq, Eq)]
     pub enum A {
         A(u32, u32),
-        B{x: u64, y: u64},
+        B { x: u64, y: u64 },
+        C(),
+        D(u32),
+        E,
     }
     impl Default for A {
         fn default() -> Self {
-            A::A(1,1)
+            A::A(1, 1)
         }
     }
-    let a_opt = A::with_prefix("o");
-    let app = a_opt.as_arguments().bind_app(clap::App::new("named_enum"));
-    let matches = app.get_matches_from_safe(&["test", "--o.B.x=2"]);
-    let a = a_opt.override_arguments(A::B{x:5,y:6}, &matches.unwrap());
-    assert_eq!(a, Some(A::B{x:2, y:6}));
+    #[test]
+    fn test_named_args_default() {
+        let a = run_test!(A, A::default(), "--o.B.x=2").unwrap();
+        assert_eq!(a, A::B { x: 2, y: 0 });
+    }
+    #[test]
+    fn test_named_args_no_override() {
+        let a = run_test!(A, A::B { x: 5, y: 6 }, "--o.B.x=2").unwrap();
+        assert_eq!(a, A::B { x: 2, y: 6 });
+    }
+    #[test]
+    fn test_empty_tuple() {
+        let a = run_test!(A, A::B { x: 5, y: 6 }, "--o.C").unwrap();
+        assert_eq!(a, A::C());
+    }
+    #[test]
+    fn test_unary_tuple() {
+        let a = run_test!(A, A::B { x: 5, y: 6 }, "--o.D.0=2").unwrap();
+        assert_eq!(a, A::D(2));
+    }
+    #[test]
+    fn test_unit() {
+        let a = run_test!(A, A::B { x: 5, y: 6 }, "--o.E").unwrap();
+        assert_eq!(a, A::E);
+    }
+}
+mod generics {
+    use prefixopt::*;
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    pub enum Either<L, R> {
+        Left { l: L },
+        Right(R),
+        None,
+        Any(),
+    }
+    impl<L: Default, R> Default for Either<L, R> {
+        fn default() -> Self {
+            Either::Left { l: L::default() }
+        }
+    }
+    #[test]
+    fn generic_enum_named() {
+        let a = run_test!(Either<u64,f64>, Either::default(), "--o.Left.l=2").unwrap();
+        assert_eq!(a, Either::Left { l: 2 });
+    }
+    #[test]
+    fn generic_unary() {
+        let a = run_test!(Either<u64,f64>, Either::default(), "--o.Right.0=2.0").unwrap();
+        assert_eq!(a, Either::Right(2.0));
+    }
+    #[test]
+    fn generic_other() {
+        let a = run_test!(Either<u64,f64>, Either::default(), "--o.None").unwrap();
+        assert_eq!(a, Either::None);
+        let a = run_test!(Either<u64,f64>, Either::default(), "--o.Any").unwrap();
+        assert_eq!(a, Either::Any());
+    }
 }
--- prefixopt_derive/tests/structs.rs
+++ prefixopt_derive/tests/structs.rs
@@ -4,55 +4,74 @@ extern crate prefixopt_derive;
 extern crate prefixopt;
 extern crate clap;
 
+macro_rules! run_test {
+    ( $ty:ty, $inp: expr, $( $args:expr),*) => {
+        {
+                let a_opt = <$ty as PrefixOpt>::with_prefix("o");
+                let app = a_opt
+                    .as_arguments()
+                    .bind_app(clap::App::new("named_enum"));
+                let matches = app.get_matches_from_safe(&["test", $($args,)*]);
+                a_opt.override_arguments($inp, &matches.unwrap())
+        }
+    }
+}
+
 #[test]
 fn named_struct() {
     use prefixopt::*;
     #[derive(PrefixOpt, Debug, PartialEq, Eq)]
     pub struct A {
-        number: u64,
+        a: u64,
         b: u32,
-        c: u16
+        c: u16,
     }
-    let ac = A::with_prefix("o");
-    let app = ac.as_arguments().bind_app(clap::App::new("named_enum"));
-    let matches = app.get_matches_from_safe(&["test", "--o.number=2", "--o.b=3"]);
-    let a = ac.override_arguments(A{number:1,b:2, c: 3}, &matches.unwrap()).unwrap();
-    assert_eq!(a, A{number:2, b:3, c:3});
+    let a = run_test!(A, A { a: 1, b: 2, c: 3 }, "--o.a=2", "--o.b=3").unwrap();
+    assert_eq!(a, A { a: 2, b: 3, c: 3 });
 }
 
 #[test]
 fn named_tuple() {
     use prefixopt::*;
     #[derive(PrefixOpt, Debug, PartialEq, Eq)]
-    pub struct A (u64,u32,u16);
-    let ac = A::with_prefix("o");
-    let app = ac.as_arguments().bind_app(clap::App::new("named_enum"));
-    let matches = app.get_matches_from_safe(&["test", "--o.0=2", "--o.1=3"]);
-    let a = ac.override_arguments(A(1, 2, 3), &matches.unwrap()).unwrap();
+    pub struct A(u64, u32, u16);
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    pub struct B(u64);
+    let a = run_test!(A, A(1, 2, 3), "--o.0=2", "--o.1=3").unwrap();
     assert_eq!(a, A(2, 3, 3));
+    let a = run_test!(B, B(1), "--o=2").unwrap();
+    assert_eq!(a, B(2));
+}
+
+#[test]
+fn fail_parse() {
+    use prefixopt::*;
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    pub struct A(u64, u32, u16);
+    let a = run_test!(A, A(1, 2, 3), "--o.0=a");
+    assert_eq!(a, None);
 }
 
 #[test]
 fn generic_struct() {
     use prefixopt::*;
-    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    #[derive(PrefixOpt, Default, Debug, PartialEq, Eq)]
     pub struct A<T> {
         a: T,
         b: T,
     }
-    impl<T: Default> Default for A<T> {
-        fn default() -> Self {
-            A {
-                a: T::default(),
-                b: T::default(),
-            }
-        }
-    }
-    let a_opt = A::<u32>::with_prefix("o");
-    let app = a_opt
-        .as_arguments()
-        .bind_app(clap::App::new("named_enum"));
-    let matches = app.get_matches_from_safe(&["test", "--o.b=2"]);
-    let a = a_opt.override_arguments(A { a: 5, b: 6 }, &matches.unwrap());
-    assert_eq!(a, Some(A { a: 5, b: 2 }));
+    let a = run_test!(A<u32>, A { a: 1, b: 2}, "--o.a=2", "--o.b=3").unwrap();
+    assert_eq!(a, A { a: 2, b: 3 });
+}
+#[test]
+fn generic_tuple() {
+    use prefixopt::*;
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    pub struct A<T>(T, u32, u16);
+    #[derive(PrefixOpt, Debug, PartialEq, Eq)]
+    pub struct B<T>(T);
+    let a = run_test!(A<f32>, A(1.,2,3), "--o.0=2", "--o.1=3").unwrap();
+    assert_eq!(a, A(2.,3,3));
+    let a = run_test!(B<f32>, B(1.), "--o=2").unwrap();
+    assert_eq!(a, B(2.));
 }
