d91d9295edbbdc16302c1e2341fc5655a602b138
Ruben Lapauw <Ruben.Lapauw@cs.kuleuven.be>
2017-11-08 12:16:13 +0100

    Cleaning up example

--- README.md
+++ README.md
@@ -29,32 +29,17 @@ pub enum A<T> {
     D(),
     E(::std::marker::PhantomData<u32>)
 }
-
-
-#[derive(Debug, PrefixOpt)]
-pub enum B {
-    Foo,
-    Bar,
-    Bux,
-}
 impl Default for A {
     fn default() -> A {
         A::A(Box::new(1), None)
     }
 }
-impl Default for B {
-    fn default() -> B {
-        B::Foo
-    }
-}
 
 fn main() {
-    let splitc = A::<B>::with_prefix("o");
-    let args = splitc.as_arguments();
-    let app = args.bind_app(clap::App::new("testing"));
-    let matches = app.get_matches();
-    let split = splitc.match_arguments(&matches);
-    println!("{:?}", split);
+    let a_opt = A::<u32>::with_prefix("o");
+    let app = a_opt.as_arguments().bind_app(clap::App::new("testing"));
+    let a = a_opt.match_arguments(&app.get_matches());
+    println!("{:?}", a);
 }
 ```
 
--- prefixopt_derive/bin/example.rs
+++ prefixopt_derive/bin/example.rs
@@ -8,34 +8,20 @@ use prefixopt::core::*;
 #[derive(Debug, PrefixOpt)]
 pub enum A<T> {
     A(Box<T>, Option<Option<u8>>),
-    B{x:B},
+    B { x: T },
     C,
     D(),
-    E(::std::marker::PhantomData<u32>)
-}
-
-#[derive(Debug, PrefixOpt)]
-pub enum B {
-    Foo,
-    Bar,
-    Bux,
+    E(::std::marker::PhantomData<u32>),
 }
 impl<T> Default for A<T> {
     fn default() -> A<T> {
         A::C
     }
 }
-impl Default for B {
-    fn default() -> B {
-        B::Foo
-    }
-}
 
 fn main() {
-    let splitc = A::<u32>::with_prefix("o");
-    let args = splitc.as_arguments();
-    let app = args.bind_app(clap::App::new("testing"));
-    let matches = app.get_matches();
-    let split = splitc.override_arguments(A::default(), &matches);
-    println!("{:?}", split);
+    let a_opt = A::<u32>::with_prefix("o");
+    let app = a_opt.as_arguments().bind_app(clap::App::new("testing"));
+    let parsed = a_opt.override_arguments(A::default(), &app.get_matches());
+    println!("{:?}", parsed);
 }
